e8616a93b84029ea564683476c1bef4cf1a3855f
njenkins2727 <njenkins2727@gmail.com>
2022-06-02 16:10:06 +0800

    updated and tidy-up

--- README.md
+++ README.md
@@ -2,8 +2,8 @@
 
 ## Description
 
-Here we have a password generator that is completely randomised. The purpose of this project is to help a user create a completely random password. I have deepened my understanding with plenty more room to improve my knowledge in Javascript.
+Here we have a password generator that is completely randomised. The purpose of this project is to help a user create a completely random password for use to access their own, online account. I have completed the Javascript  functions within this app. This process has deepened my understanding within Javascript. 
 
- ![ImageOfWebsite](./ss-gen-password.pdf)
+ ![Image Of Website](./127.0.0.1_5500_index.html.png);
 
  ### https://njenkins2727.github.io/random-password-generator/
--- script.js
+++ script.js
@@ -9,6 +9,7 @@ const numericList = [0, 1 , 2, 3, 4, 5, 6, 7, 8, 9]
 
 const specialCharactersList = ['@','%','+','\\','/',"'",'!','#','$','^','?',':',',',')',]
 
+
 // Write password to the #password input
 function writePassword() {
   const password = generatePassword();
@@ -22,20 +23,24 @@ generateBtn.addEventListener("click", writePassword);
 
 function generatePassword(){
 
+
  // Please edit your password criteria (confirm)
 const firstPopup = confirm("Please edit your password criteria");
 if (firstPopup === false){
   return "";
 }
 
+
  //how many characters in your password. 8-128 characters only(prompt)
  const charLength = parseInt(prompt("How many characters in your password. 8-128 characters only"));
 
+
  //validate 8>128
  if (charLength<8 || charLength>128 || isNaN(charLength) ){
-   alert("Invalid");
+   alert("Invalid: must be 8 - 128 characters");
    return "Please try again."
  } 
+
 
 //ask what 'charType' to include in password (confirm) : lowercase, uppercase, numeric, and/or special characters
 const lowerCaseType = confirm("Do you want 'lowercase' characters in your password");
@@ -46,11 +51,12 @@ const numericType = confirm("Do you want 'numeric' characters in your password")
 
 const specialCharType = confirm("Do you want 'special' characters in your password");
 
+
 //validate input (min one selected)
 let allChoices = []
 
 if (!lowerCaseType && !upperCaseType && !numericType && !specialCharType){
-  alert("wrong");
+  alert("Must confirm atleast ONE password criteria.");
   return "try again";
 }
 
@@ -70,7 +76,10 @@ if (specialCharType === true){
   allChoices = allChoices.concat(specialCharactersList)
 } 
 
-console.log(allChoices); // testing allChoices
+
+// testing allChoices
+console.log(allChoices); 
+
 
 //generate password with validated criteria 
 let createPassword = '';
@@ -78,8 +87,11 @@ let createPassword = '';
 for (let index = 0; index < charLength; index++) {
    createPassword += allChoices[Math.floor(Math.random()*allChoices.length)];
 }
+
+
 //display password in writePassword function 
   return createPassword;
+
 }
 
 
